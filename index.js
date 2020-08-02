12738540442a0e26257f77dabbd1e8b47e2c410c
Wjatscheslaw Poluschin <w.poluschin@ctimeetingtech.com>
2020-08-02 13:41:49 +0200

    Add @aws-amplify/analytics to bundle

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import { Amplify } from "@aws-amplify/core/lib-esm"
 import { Auth, CognitoUser, CookieStorage, CognitoHostedUIIdentityProvider, appendToCognitoUserAgent } from "@aws-amplify/auth/lib-esm"
 import { DataStore, initSchema, Predicates } from "@aws-amplify/datastore/lib-esm"
 import { API, graphqlOperation } from "@aws-amplify/api/lib-esm"
+import { Analytics, AWSPinpointProvider, AmazonPersonalizeProvider, AWSKinesisFirehoseProvider, AWSKinesisProvider } from "@aws-amplify/analytics/lib-esm"
 
 export {
-  Amplify, Auth, CognitoUser, CookieStorage, CognitoHostedUIIdentityProvider, appendToCognitoUserAgent,
-  DataStore, initSchema, Predicates, API, graphqlOperation
+  Amplify,
+  Auth, CognitoUser, CookieStorage, CognitoHostedUIIdentityProvider, appendToCognitoUserAgent,
+  DataStore, initSchema, Predicates,
+  API, graphqlOperation,
+  Analytics, AWSPinpointProvider, AmazonPersonalizeProvider, AWSKinesisFirehoseProvider, AWSKinesisProvider
 }
 
 // Datastore depends on a all of Amplify JS libraries (core, auth, api, pubsub, cache). There's no point tree shaking
