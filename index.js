bb62dc0603a1f89110f817b51c63755978a3e693
Wjatscheslaw Poluschin <w.poluschin@ctimeetingtech.com>
2020-08-26 09:36:53 +0200

    Export ConsoleLogger, Storage, StorageProvider

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Amplify } from "@aws-amplify/core/lib-esm"
+import { Amplify, ConsoleLogger } from "@aws-amplify/core/lib-esm"
 import { Auth, CognitoUser, CookieStorage, CognitoHostedUIIdentityProvider, appendToCognitoUserAgent } from "@aws-amplify/auth/lib-esm"
 import { DataStore, initSchema, Predicates } from "@aws-amplify/datastore/lib-esm"
 import { API, graphqlOperation } from "@aws-amplify/api/lib-esm"
@@ -6,11 +6,12 @@ import { Analytics, AWSPinpointProvider, AmazonPersonalizeProvider, AWSKinesisFi
 import { Storage, StorageProvider } from "@aws-amplify/storage/lib-esm"
 
 export {
-  Amplify,
+  Amplify, ConsoleLogger,
   Auth, CognitoUser, CookieStorage, CognitoHostedUIIdentityProvider, appendToCognitoUserAgent,
   DataStore, initSchema, Predicates,
   API, graphqlOperation,
-  Analytics, AWSPinpointProvider, AmazonPersonalizeProvider, AWSKinesisFirehoseProvider, AWSKinesisProvider
+  Analytics, AWSPinpointProvider, AmazonPersonalizeProvider, AWSKinesisFirehoseProvider, AWSKinesisProvider,
+  Storage, StorageProvider,  
 }
 
 // Datastore depends on a all of Amplify JS libraries (core, auth, api, pubsub, cache). There's no point tree shaking
